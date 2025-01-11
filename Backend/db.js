84e90268ef175504bb224d79c924d647b4ea927c
Chandan-kumar283 <chandan020803@gmail.com>
2025-01-11 10:34:17 +0530

    Updated db.js

--- Backend/db.js
+++ Backend/db.js
@@ -1,5 +1,11 @@
 const mongoose = require('mongoose')
-const mongoURI = "mongodb+srv://chandankr:llat0LACLDK8fz3j@notes.6evbw.mongodb.net/Notebook"
+require('dotenv').config();
+
+const mongoURI = process.env.MONGO_URI;
+
+console.log(`MongoDB URI: ${process.env.MONGO_URI.replace(/:\/\/.*@/, '://***:***@')}`);
+;
+
 const connectToMongo = async()=>{
   await mongoose.connect(mongoURI);
         console.log(`connected to mongoDB sucessfully ${mongoose.connection.host}`)
